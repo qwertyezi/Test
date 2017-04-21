78a09f1e57d2abd8542af45ff4f538157303192f
yezi <luyezhang@loopeer.com>
2017-04-21 18:45:27 +0800

    add something

--- text/TestMedia/src/main/res/raw/beauty_vertex.glsl
+++ text/TestMedia/src/main/res/raw/beauty_vertex.glsl
@@ -1,34 +1,34 @@
-attribute vec4 vPosition;
-attribute vec2 vCoord;
-varying vec2 textureCoordinate;
-varying vec2 blurCoord1s[14];
+attribute vec4 aPosition;
+attribute vec2 aCoord;
+varying vec2 vTextureCoordinate;
+varying vec2 vBlurCoord1s[14];
 const highp float mWidth=720.0;
 const highp float mHeight=1280.0;
-uniform mat4 vMatrix;
+uniform mat4 uMatrix;
 void main( )
 {
-    gl_Position = vMatrix*vPosition;
-    textureCoordinate = vCoord;
+    gl_Position = uMatrix*aPosition;
+    vTextureCoordinate = aCoord;
 
     highp float mul_x = 2.0 / mWidth;
     highp float mul_y = 2.0 / mHeight;
 
-    blurCoord1s[0] = textureCoordinate + vec2( 0.0 * mul_x, -10.0 * mul_y );
-    blurCoord1s[1] = textureCoordinate + vec2( 8.0 * mul_x, -5.0 * mul_y );
-    blurCoord1s[2] = textureCoordinate + vec2( 8.0 * mul_x, 5.0 * mul_y );
-    blurCoord1s[3] = vCoord + vec2( 0.0 * mul_x, 10.0 * mul_y );
-    blurCoord1s[4] = vCoord + vec2( -8.0 * mul_x, 5.0 * mul_y );
-    blurCoord1s[5] = vCoord + vec2( -8.0 * mul_x, -5.0 * mul_y );
+    vBlurCoord1s[0] = vTextureCoordinate + vec2( 0.0 * mul_x, -10.0 * mul_y );
+    vBlurCoord1s[1] = vTextureCoordinate + vec2( 8.0 * mul_x, -5.0 * mul_y );
+    vBlurCoord1s[2] = vTextureCoordinate + vec2( 8.0 * mul_x, 5.0 * mul_y );
+    vBlurCoord1s[3] = aCoord + vec2( 0.0 * mul_x, 10.0 * mul_y );
+    vBlurCoord1s[4] = aCoord + vec2( -8.0 * mul_x, 5.0 * mul_y );
+    vBlurCoord1s[5] = aCoord + vec2( -8.0 * mul_x, -5.0 * mul_y );
 
     mul_x = 1.2 / mWidth;
     mul_y = 1.2 / mHeight;
 
-    blurCoord1s[6] = vCoord + vec2( 0.0 * mul_x, -6.0 * mul_y );
-    blurCoord1s[7] = vCoord + vec2( -4.0 * mul_x, -4.0 * mul_y );
-    blurCoord1s[8] = vCoord + vec2( -6.0 * mul_x, 0.0 * mul_y );
-    blurCoord1s[9] = vCoord + vec2( -4.0 * mul_x, 4.0 * mul_y );
-    blurCoord1s[10] = vCoord + vec2( 0.0 * mul_x, 6.0 * mul_y );
-    blurCoord1s[11] = vCoord + vec2( 4.0 * mul_x, 4.0 * mul_y );
-    blurCoord1s[12] = vCoord + vec2( 6.0 * mul_x, 0.0 * mul_y );
-    blurCoord1s[13] = vCoord + vec2( 4.0 * mul_x, -4.0 * mul_y );
+    vBlurCoord1s[6] = aCoord + vec2( 0.0 * mul_x, -6.0 * mul_y );
+    vBlurCoord1s[7] = aCoord + vec2( -4.0 * mul_x, -4.0 * mul_y );
+    vBlurCoord1s[8] = aCoord + vec2( -6.0 * mul_x, 0.0 * mul_y );
+    vBlurCoord1s[9] = aCoord + vec2( -4.0 * mul_x, 4.0 * mul_y );
+    vBlurCoord1s[10] = aCoord + vec2( 0.0 * mul_x, 6.0 * mul_y );
+    vBlurCoord1s[11] = aCoord + vec2( 4.0 * mul_x, 4.0 * mul_y );
+    vBlurCoord1s[12] = aCoord + vec2( 6.0 * mul_x, 0.0 * mul_y );
+    vBlurCoord1s[13] = aCoord + vec2( 4.0 * mul_x, -4.0 * mul_y );
 }
